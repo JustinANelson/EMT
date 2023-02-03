9c329d2e58f2b5bbb7bab1eeda200f4d7fda3e39
jnels <jnelsong1@gmail.com>
2023-02-02 20:06:06 -0500

    Initial commit

---
+++ android/src/main/java/com/forgottenartsstudios/emt/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.forgottenartsstudios.emt.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.forgottenartsstudios.emt.emt;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new emt(), configuration);
+    }
+}
